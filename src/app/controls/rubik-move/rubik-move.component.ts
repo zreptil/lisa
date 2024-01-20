db13b294a1cc677de5e92cda2ab503efa27e2ec7
Andi <andi@zreptil.de>
2024-01-20 07:41:44 +0100

    rubik further implemented

--- src/app/controls/rubik-move/rubik-move.component.ts
+++ src/app/controls/rubik-move/rubik-move.component.ts
@@ -14,28 +14,33 @@ export class RubikMoveComponent {
   onMove = new EventEmitter<string>();
   @Input()
   reversed = false;
+  @Input()
+  name: string;
+
   cube: RubikCube = new RubikCube();
   cfg: any = {
-    D: {icon: 'east', mc: 'bottom', face: 'f'},
-    d: {icon: 'west', mc: 'bottom', face: 'f'},
-    F: {icon: 'replay', ic: 'left', mc: 'center', face: 'f'},
-    f: {icon: 'replay', ic: 'right', mc: 'center', face: 'f'},
-    L: {icon: 'east', mc: 'left', ic: 'down', face: 'f'},
-    l: {icon: 'east', mc: 'left', ic: 'up', face: 'f'},
-    R: {icon: 'east', mc: 'right', ic: 'up', face: 'f'},
-    r: {icon: 'east', mc: 'right', ic: 'down', face: 'f'},
-    U: {icon: 'west', mc: 'top', face: 'f'},
-    u: {icon: 'east', mc: 'top', face: 'f'},
-    E: {icon: 'east', face: 'f'},
-    e: {icon: 'west', face: 'f'},
-    M: {icon: 'east', ic: 'down', face: 'f'},
-    m: {icon: 'east', ic: 'up', face: 'f'},
-    S: {icon: 'west', ic: 'up', face: 'r'},
-    s: {icon: 'west', ic: 'down', face: 'r'},
-    B: {icon: 'west', ic: 'right', mc: 'top', face: 'u'},
-    b: {icon: 'west', ic: 'left', mc: 'top', face: 'u'},
+    D: {icon: 'chevron_right', mc: 'bottom', face: 'f', 2: 'keyboard_double_arrow_right'},
+    d: {icon: 'chevron_left', mc: 'bottom', face: 'f', 2: 'keyboard_double_arrow_left'},
+    F: {icon: 'replay', ic: 'left', mc: 'center', face: 'f', 2: 'sync'},
+    f: {icon: 'replay', ic: 'right', mc: 'center', face: 'f', 2: 'sync'},
+    L: {icon: 'chevron_right', mc: 'left', ic: 'down', face: 'f', 2: 'keyboard_double_arrow_right'},
+    l: {icon: 'chevron_right', mc: 'left', ic: 'up', face: 'f', 2: 'keyboard_double_arrow_right'},
+    R: {icon: 'chevron_right', mc: 'right', ic: 'up', face: 'f', 2: 'keyboard_double_arrow_right'},
+    r: {icon: 'chevron_right', mc: 'right', ic: 'down', face: 'f', 2: 'keyboard_double_arrow_right'},
+    U: {icon: 'chevron_left', mc: 'top', face: 'f', 2: 'keyboard_double_arrow_left'},
+    u: {icon: 'chevron_right', mc: 'top', face: 'f', 2: 'keyboard_double_arrow_right'},
+    E: {icon: 'chevron_right', face: 'f', 2: 'keyboard_double_arrow_right'},
+    e: {icon: 'chevron_left', face: 'f', 2: 'keyboard_double_arrow_left'},
+    M: {icon: 'chevron_right', ic: 'down', face: 'f', 2: 'keyboard_double_arrow_right'},
+    m: {icon: 'chevron_right', ic: 'up', face: 'f', 2: 'keyboard_double_arrow_right'},
+    S: {icon: 'chevron_left', ic: 'up', face: 'r', 2: 'keyboard_double_arrow_left'},
+    s: {icon: 'chevron_left', ic: 'down', face: 'r', 2: 'keyboard_double_arrow_left'},
+    B: {icon: 'chevron_left', ic: 'right', mc: 'top', face: 'u', 2: 'keyboard_double_arrow_left'},
+    b: {icon: 'chevron_left', ic: 'left', mc: 'top', face: 'u', 2: 'keyboard_double_arrow_left'},
   };
-
+  // keyboard_double_arrow_right
+  // keyboard_double_arrow_left
+  // sync
   showMoves = false;
 
   constructor(public rs: RubikService) {
@@ -58,14 +63,23 @@ export class RubikMoveComponent {
 
   get moves(): string[] {
     const ret = [];
+    const temp = [];
     var i: number;
     if (this.reversed) {
       for (i = this.move?.length - 1; i >= 0; i--) {
-        ret.push(Utils.toggleCase(this.move[i]));
+        temp.push(Utils.toggleCase(this.move[i]));
       }
     } else {
       for (i = 0; i < this.move?.length; i++) {
-        ret.push(this.move[i]);
+        temp.push(this.move[i]);
+      }
+    }
+    for (i = 0; i < temp.length; i++) {
+      if (i < temp.length - 1 && temp[i] === temp[i + 1]) {
+        ret.push(`${temp[i]}2`);
+        i++;
+      } else {
+        ret.push(temp[i]);
       }
     }
     return ret;
@@ -106,22 +120,25 @@ export class RubikMoveComponent {
   }
 
   faceForMove(moveId: string): string {
-    return this.cfg[moveId]?.face;
+    return this.cfg[moveId[0]]?.face;
   }
 
   iconForMove(moveId: string): string {
+    if (moveId.length === 2 && moveId[1] === '2') {
+      return this.cfg[moveId[0]]?.['2'];
+    }
     return this.cfg[moveId]?.icon;
   }
 
   classForMove(moveId: string): string[] {
     const ret: string[] = [];
-    ret.push(this.cfg[moveId]?.mc);
+    ret.push(this.cfg[moveId[0]]?.mc);
     return ret;
   }
 
   classForIcon(moveId: string): string[] {
     const ret: string[] = [];
-    ret.push(this.cfg[moveId]?.ic);
+    ret.push(this.cfg[moveId[0]]?.ic);
     return ret;
   }
 
